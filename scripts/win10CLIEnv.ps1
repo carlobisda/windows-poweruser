9c084f5fb649cfdc36d0d3133979e2270eee9392
carlobisda <38801646+carlobisda@users.noreply.github.com>
2022-07-17 01:27:13 -0400

    Add files via upload

---
+++ scripts/win10CLIEnv.ps1
@@ -0,0 +1,11 @@
+﻿# win10CLIEnv
+# WIP A powershell script that creates a CLI environment
+
+taskkill.exe /f /im "explorer.exe"
+Write-Output initializing l33t mode.....
+invoke-item a:\
+Write-Output initializing filemon.exe.....
+start-process powershell
+Write-Output initializing file p0w3rsh3ll......
+
+# thank you zuper-cb v3.5
