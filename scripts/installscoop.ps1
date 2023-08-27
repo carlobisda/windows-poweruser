a3a3247052ec5e7769669aee382619ed86028b91
carlobisda <38801646+carlobisda@users.noreply.github.com>
2023-08-27 17:25:58 -0400

    added glogg

--- scripts/installscoop.ps1
+++ scripts/installscoop.ps1
@@ -9,8 +9,8 @@ write-debug "scoop buckets has been installed"
 write-debug "installing apps....."
 
 scoop install googlechrome
+scoop install firefox
 scoop install powertoys
-scoop install nvidia-profile-inspector
 scoop install sidebar-diagnostics
 scoop install k-lite-codec-pack-mega-np
 scoop install neofetch
@@ -19,10 +19,6 @@ scoop install barrier-np
 scoop install vscode-insiders
 scoop install windows-terminal
 scoop install virtualbox-np
-scoop install islc
-scoop install processactivityview
-scoop install currprocess
-scoop install processhacker
 scoop install notepadplusplus
 scoop install mpc-qt
 scoop install vlc
@@ -37,14 +33,10 @@ scoop install qbittorrent
 scoop install audacity
 scoop install lmms
 scoop install balabolka
-scoop install cpu-z
-scoop install gpu-z
 scoop install open-log-viewer
 scoop install komorebi
 scoop install workspacer
 scoop install 7zip
-scoop install msiafterburner
-scoop install msikombustor
 scoop install chatgpt
 scoop install gimp
 scoop install blender
@@ -55,6 +47,19 @@ scoop install irfanview
 scoop install git
 scoop install lynx
 scoop install puttie
+scoop install ffmpeg
+scoop install glogg
+
+scoop install msiafterburner
+scoop install msikombustor
+scoop install aida64extreme
+scoop install cpu-z
+scoop install gpu-z
+scoop install islc
+scoop install processactivityview
+scoop install currprocess
+scoop install processhacker
+scoop install nvidia-profile-inspector
 
 scoop install minetest
 scoop install steam
@@ -66,5 +71,6 @@ scoop install mesen
 scoop install ppsspp
 scoop install bluestacks-np
 scoop install duckstation
+scoop install playnite
 
 write-debug "scoop install dwm_lut"
