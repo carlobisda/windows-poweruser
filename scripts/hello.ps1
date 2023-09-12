04b1e3364bd701f76c17d44febe481489e5d8f8c
carlobisda <38801646+carlobisda@users.noreply.github.com>
2023-09-12 00:06:31 -0400

    startup script

---
+++ scripts/hello.ps1
@@ -0,0 +1,18 @@
+Clear-Host
+$fortune = "C:\Users\carlo\Downloads\glitch\cookie.exe" 
+Invoke-Expression $fortune
+Start-Sleep -Seconds 5
+
+Clear-Host
+$fortunecookie = "C:\Users\carlo\Downloads\glitch\rando.exe wisdom.pick" 
+Invoke-Expression $fortunecookie
+Start-Sleep -Seconds 5
+
+Clear-Host
+Write-Host "Be The Monster"
+Start-Sleep -Seconds 3
+$tts = "C:\Users\carlo\Downloads\glitch\tts.exe no sleep in brooklyn " 
+Invoke-Expression $tts
+
+Clear-Host
+Get-Content C:\Users\carlo\Downloads\glitch\keybinds.md 
