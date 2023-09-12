8e0a95e5414a97592c94c833ccc0b0251598355a
carlobisda <38801646+carlobisda@users.noreply.github.com>
2023-09-12 00:05:38 -0400

    neofetch script refresh

---
+++ scripts/fetch.ps1
@@ -0,0 +1,26 @@
+#neofetch must be installed for this to work
+
+Clear-Host
+$fetch = "neofetch" 
+Invoke-Expression $fetch
+Start-Sleep -Seconds 18000
+
+Clear-Host
+$fetch = "neofetch" 
+Invoke-Expression $fetch
+Start-Sleep -Seconds 18000
+
+Clear-Host
+$fetch = "neofetch" 
+Invoke-Expression $fetch
+Start-Sleep -Seconds 18000
+
+Clear-Host
+$fetch = "neofetch" 
+Invoke-Expression $fetch
+Start-Sleep -Seconds 18000
+
+Clear-Host
+$fetch = "neofetch" 
+Invoke-Expression $fetch
+Start-Sleep -Seconds 18000
